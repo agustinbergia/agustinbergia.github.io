8488a464c628c6005086a53717da8b5722e7dc9e
agustin <agustin.bergia@gmail.com>
2023-01-19 18:57:26 -0300

    first commit and changes

---
+++ build.sh
@@ -0,0 +1,5 @@
+rm -Rf /home/ubuntu/agustinbergia.github.io/_site
+cd /home/ubuntu/agustinbergia.github.io/
+bundle exec jekyll build
+cd /home/ubuntu/agustinbergia.github.io/_site
+bundle exec jekyll serve
